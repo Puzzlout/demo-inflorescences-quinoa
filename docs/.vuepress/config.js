649d902928e03759e189e599b25b7444f6ade698
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-05-12 07:59:55 +0200

    bump vuepress to beta 62 and theme to beta 210

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-const version = '1.0.23';
+const version = '1.0.24';
 
 import { defineUserConfig } from '@vuepress/cli';
 import { viteBundler } from '@vuepress/bundler-vite';
--- docs/.vuepress/configs/head/en.js
+++ docs/.vuepress/configs/head/en.js
@@ -1,9 +1,9 @@
-const websiteDomain = 'template-vuepress-next.netlify.app';
-const titleDefault = 'Demo of static Vuepress website';
+const websiteDomain = 'demo-inflorescences.netlify.app';
+const titleDefault = 'Demo - Inflorescences Quinoa';
 const descriptionDefault =
-  'A template Vuepress blog using Mr Hope very feature-rich theme';
-const themeColor = '#46b282';
-const repoLink = 'https://github.com/Puzzlout/TemplateVuepress';
+  'Quinoa durable, produit en Normandie depuis 2021 par Inflorescences.';
+const themeColor = '#BD512F';
+const repoLink = 'https://github.com/Puzzlout/demo-inflorescences-quinoa';
 
 export const enHeadConfig = {
   title: titleDefault,
